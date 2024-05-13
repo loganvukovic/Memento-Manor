8579942c46f03cc83c67b8006f339222d47244ad
SteveMinecraf <167727099+BigShotGameing@users.noreply.github.com>
2024-05-13 16:03:12 -0400

    made changes to camera and made level bits

--- Most Important Vase/Assets/Scripts/PlayerMovement.cs
+++ Most Important Vase/Assets/Scripts/PlayerMovement.cs
@@ -58,7 +58,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        camera.transform.position = new Vector3(transform.position.x, transform.position.y, camera.transform.position.z);
+        //camera.transform.position = new Vector3(transform.position.x, transform.position.y, camera.transform.position.z);
 
         if (!inDialogue) 
         {
