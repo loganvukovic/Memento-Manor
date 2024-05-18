c45ae0ae22a597ab80d0ffc78675d56d449e2a72
SteveMinecraf <167727099+BigShotGameing@users.noreply.github.com>
2024-05-18 00:22:48 -0400

    Added teleporting elevator

---
+++ Most Important Vase/Assets/Scripts/TeleportPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour
+{
+    // Reference to the teleport destination
+    public Transform teleportDestination;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the player enters the trigger area
+        if (other.CompareTag("Player"))
+        {
+            // Teleport the player to the destination
+            other.transform.position = teleportDestination.position;
+        }
+    }
+}
